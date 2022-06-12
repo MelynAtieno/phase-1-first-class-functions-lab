6f64702bbdfb18e758f7306e964994a8d1b9fef9
Melyn Atieno Were <melynatieno@gmail.com>
2022-06-12 20:46:18 +0300

    commit tree ff996ee9b02f4904ae32ab973db97f026981f7a4

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(-2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function (multiply) {
+    return function (num) {
+      return multiply * num;
+    };
+  };
+
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+        
+const selectDifferentDrivers = function (drivers, driverNames) {
+    return driverNames(drivers);
+  };   
+
+
